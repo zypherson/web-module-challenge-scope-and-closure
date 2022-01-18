4799342b79681b63ecb83c8a2850dea8de1fb3b4
zypherson <taylorzypherson@gmail.com>
2022-01-17 20:47:35 -0500

    Completed MVP

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()* 3)
 }
 
 
@@ -81,17 +81,32 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
-}
+function finalScore(inningcb, innings){
+  let homeScore = 0;
+  let awayScore = 0;
+  for(let i = 0; i < innings; i++){
+    homeScore = homeScore + inning()
+    awayScore = awayScore + inning()
+  }
+  return {
+    Home: homeScore ,
+    Away: awayScore
+      }
+  }
+console.log(finalScore(inning,9))
+
+console.log(finalScore(inning(8)))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inning(),
+    Away: inning()
+  }
 }
 
 
